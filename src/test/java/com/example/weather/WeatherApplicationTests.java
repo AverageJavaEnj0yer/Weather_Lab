fb11d950f9c344f31f18ad5bbbb7faf3213994ce
danik <danik@DESKTOP-AIHBFQV>
2024-02-18 18:01:28 +0300

    тест добавлен

--- src/test/java/com/example/weather/WeatherApplicationTests.java
+++ src/test/java/com/example/weather/WeatherApplicationTests.java
@@ -2,38 +2,30 @@ package com.example.weather;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.hamcrest.Matchers.containsString;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WeatherApplicationTests {
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate restTemplate;
 
     @Test
-    void testGetWeatherByCity() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/weather")
-                        .param("city", "London"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(containsString("прогноз погоды")));
+    void testGetWeatherByCity() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/weather?city=London", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("прогноз погоды");
     }
 
     @Test
-    void testGetWeatherByCoordinates() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/weatherByCoordinates")
-                        .param("lat", "51.5074")
-                        .param("lon", "0.1278"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(containsString("прогноз погоды")));
+    void testGetWeatherByCoordinates() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/weatherByCoordinates?lat=51.5074&lon=0.1278", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("прогноз погоды");
     }
 }
-
-
