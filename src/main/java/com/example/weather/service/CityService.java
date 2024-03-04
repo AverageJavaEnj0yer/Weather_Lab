1d9cb744eede1f9033e38452462a5bc1419832f4
danik <danik@DESKTOP-AIHBFQV>
2024-03-04 18:29:37 +0300

    БД

--- src/main/java/com/example/weather/service/CityService.java
+++ src/main/java/com/example/weather/service/CityService.java
@@ -4,6 +4,7 @@ import com.example.weather.entity.City;
 import com.example.weather.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -20,12 +21,22 @@ public class CityService {
     }
 
     public City createCity(City city) {
+        // Проверяем, существует ли уже город с таким именем
+        if (cityRepository.existsByName(city.getName())) {
+            // Если город существует, можно выбросить исключение или вернуть null
+            // Здесь выбрасывается исключение, но можно использовать и другие подходы
+            throw new RuntimeException("Город с таким именем уже существует");
+        }
+        // Если город с таким именем не существует, сохраняем его
         return cityRepository.save(city);
     }
 
     public City updateCity(Long id, City newCityData) {
         City cityToUpdate = cityRepository.findById(id).orElse(null);
         if (cityToUpdate != null) {
+            if (cityRepository.existsByNameAndIdNot(newCityData.getName(), id)) {
+                throw new RuntimeException("Город с таким именем уже существует");
+            }
             cityToUpdate.setName(newCityData.getName());
             // Другие поля для обновления...
             return cityRepository.save(cityToUpdate);
