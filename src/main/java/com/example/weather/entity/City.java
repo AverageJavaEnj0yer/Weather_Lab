d2b1801f89edc1b086e016823f0fbd1ac43d9792
danik <danik@DESKTOP-AIHBFQV>
2024-03-05 11:03:57 +0300

    Мега Безопасность

--- src/main/java/com/example/weather/entity/City.java
+++ src/main/java/com/example/weather/entity/City.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
+//y
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class City {
