582556f03602c47cc606630e4ffaae167377fc1c
danik <danik@DESKTOP-AIHBFQV>
2024-03-09 18:27:15 +0300

    СУПЕР ИМБА. ШОК+++++++++++

--- src/main/java/com/example/weather/cache/WeatherDataCache.java
+++ src/main/java/com/example/weather/cache/WeatherDataCache.java
@@ -12,12 +12,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class WeatherDataCache {
-    private static final int maxCacheSize = 1; // Максимальное количество записей в кэше
+    private static final int MAX_CACHE_SIZE = 1; // Максимальное количество записей в кэше
     private final Map<LocalDate, List<City>> cache = new ConcurrentHashMap<>();
     private final LinkedList<LocalDate> accessOrder = new LinkedList<>();
 
     public synchronized void addToCache(LocalDate date, List<City> cities) {
-        if (cache.size() >= maxCacheSize) {
+        if (cache.size() >= MAX_CACHE_SIZE) {
             LocalDate oldestDate = accessOrder.pollFirst();
             cache.remove(oldestDate);
         }
