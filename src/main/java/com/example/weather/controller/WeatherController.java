774f3e2fd3ac5e528e1bdc7122d9f02a8c6a686a
danik <danik@DESKTOP-AIHBFQV>
2024-03-10 19:35:30 +0300

    БОЛЬШЕ ЛОГОВ!!!

--- src/main/java/com/example/weather/controller/WeatherController.java
+++ src/main/java/com/example/weather/controller/WeatherController.java
@@ -8,10 +8,13 @@ import com.example.weather.service.CityService;
 import com.example.weather.service.WeatherConditionService;
 import com.example.weather.service.WeatherService;
 import com.example.weather.model.WeatherApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +26,8 @@ public class WeatherController {
     private final CityService cityService;
     private final WeatherConditionService weatherConditionService;
 
+    private static final Logger logger = LoggerFactory.getLogger(WeatherController.class);
+
     public WeatherController(WeatherService weatherService, CityService cityService, WeatherConditionService weatherConditionService) {
         this.weatherService = weatherService;
         this.cityService = cityService;
@@ -31,22 +36,28 @@ public class WeatherController {
 
     @GetMapping("/weather")
     public ResponseEntity<Object> getWeatherByCity(@RequestParam String city) {
+        logger.info("Fetching weather data by city: {}", city);
         WeatherApiResponse apiResponse = weatherService.getWeatherByCity(city);
         if (apiResponse != null) {
             saveWeatherData(apiResponse);
+            logger.info("Weather data fetched successfully for city: {}", city);
             return ResponseEntity.ok(apiResponse);
         } else {
+            logger.error("Failed to fetch weather data for city: {}", city);
             return ResponseEntity.notFound().build();
         }
     }
 
     @GetMapping("/weatherByCoordinates")
     public ResponseEntity<Object> getWeatherByCoordinates(@RequestParam String lat, @RequestParam String lon) {
+        logger.info("Fetching weather data by coordinates. Latitude: {}, Longitude: {}", lat, lon);
         WeatherApiResponse apiResponse = weatherService.getWeatherByCoordinates(lat, lon);
         if (apiResponse != null) {
             saveWeatherData(apiResponse);
+            logger.info("Weather data fetched successfully for coordinates. Latitude: {}, Longitude: {}", lat, lon);
             return ResponseEntity.ok(apiResponse);
         } else {
+            logger.error("Failed to fetch weather data for coordinates. Latitude: {}, Longitude: {}", lat, lon);
             return ResponseEntity.notFound().build();
         }
     }
--- src/main/java/com/example/weather/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/weather/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.weather.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,13 +12,24 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(CityAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ResponseEntity<ErrorResponse> handleCityAlreadyExistsException(CityAlreadyExistsException ex) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "City with this name already exists. Please provide a different city name.");
+        logger.error("City Already Exists Exception: {}", ex.getMessage(), ex);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
-
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ResponseEntity<ErrorResponse> handleInternalServerError(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error. Please try again later or contact the administrator.");
+        logger.error("Internal Server Error: {}", ex.getMessage(), ex);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
+
