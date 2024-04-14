5f0d3445a5081f7cf2373478b705772cb77284fa
danik <danik@DESKTOP-AIHBFQV>
2024-04-15 01:28:35 +0300

    ++

--- init.sql
+++ init.sql
@@ -1,32 +1,30 @@
-CREATE TABLE IF NOT EXISTS City (
-                                    id SERIAL PRIMARY KEY,
-                                    name VARCHAR(255) NOT NULL,
-                                    lon DOUBLE PRECISION NOT NULL,
-                                    lat DOUBLE PRECISION NOT NULL
+
+CREATE TABLE City (
+                      id SERIAL PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL,
+                      lon DOUBLE PRECISION NOT NULL,
+                      lat DOUBLE PRECISION NOT NULL
 );
 
--- Создание таблицы WeatherCondition
-CREATE TABLE IF NOT EXISTS WeatherCondition (
-                                                id SERIAL PRIMARY KEY,
-                                                main VARCHAR(255) NOT NULL,
-                                                description VARCHAR(255) NOT NULL,
-                                                icon VARCHAR(255) NOT NULL
+CREATE TABLE WeatherCondition (
+                                  id SERIAL PRIMARY KEY,
+                                  main VARCHAR(255) NOT NULL,
+                                  description VARCHAR(255) NOT NULL,
+                                  icon VARCHAR(255) NOT NULL
 );
 
--- Создание таблицы WeatherData
-CREATE TABLE IF NOT EXISTS WeatherData (
-                                           id SERIAL PRIMARY KEY,
-                                           date DATE NOT NULL,
-                                           temperature DOUBLE PRECISION NOT NULL,
-                                           humidity DOUBLE PRECISION NOT NULL,
-                                           city_id INT,
-                                           FOREIGN KEY (city_id) REFERENCES City(id)
+CREATE TABLE WeatherData (
+                             id SERIAL PRIMARY KEY,
+                             date DATE NOT NULL,
+                             temperature DOUBLE PRECISION NOT NULL,
+                             humidity DOUBLE PRECISION NOT NULL,
+                             city_id INT,
+                             FOREIGN KEY (city_id) REFERENCES City(id)
 );
 
--- Создание связи между таблицами WeatherData и WeatherCondition
-CREATE TABLE IF NOT EXISTS WeatherData_WeatherCondition (
-                                                            weatherData_id INT,
-                                                            weatherCondition_id INT,
-                                                            FOREIGN KEY (weatherData_id) REFERENCES WeatherData(id),
-                                                            FOREIGN KEY (weatherCondition_id) REFERENCES WeatherCondition(id)
+CREATE TABLE WeatherData_WeatherCondition (
+                                              weatherData_id INT,
+                                              weatherCondition_id INT,
+                                              FOREIGN KEY (weatherData_id) REFERENCES WeatherData(id),
+                                              FOREIGN KEY (weatherCondition_id) REFERENCES WeatherCondition(id)
 );
